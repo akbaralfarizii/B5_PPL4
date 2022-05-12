229c5046ab3424e5c877274c96c07382c6e3a9bf
HanhanSeptians <hanhan.septian.tif20@polban.ac.id>
2022-05-12 09:23:54 +0700

    tiga halaman

--- FrontEnd/FPTLN/src/views/administrasiusers/AdministrasiUsers.js
+++ FrontEnd/FPTLN/src/views/administrasiusers/AdministrasiUsers.js
@@ -1,9 +1,90 @@
 import React from 'react'
+import {
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CRow,
+  CTable,
+  CTableBody,
+  CTableCaption,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+  CButton
+} from '@coreui/react'
+import { DocsCallout, DocsExample } from 'src/components'
 
 const AdministrasiUsers = () => {
   return (
     <>
-      <p> AdministrasiUsers</p>
+      <DocsExample>
+        <CTable striped>
+          <CTableHead>
+            <CTableRow>
+              <CTableHeaderCell scope="col">No</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Edit Hak Akses</CTableHeaderCell>
+              <CTableHeaderCell scope="col">NIP</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Nama</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Password</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Akses</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Administrasi Users</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Master</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Daftar FP</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Nilai FP</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Report</CTableHeaderCell>
+            </CTableRow>
+          </CTableHead>
+          <CTableBody>
+            <CTableRow>
+              <CTableDataCell>1</CTableDataCell>
+              <CTableDataCell>
+                <CButton className='info'>Edit akses</CButton>
+              </CTableDataCell>
+              <CTableDataCell>201511041</CTableDataCell>
+              <CTableDataCell>Hanhan Septian</CTableDataCell>
+              <CTableDataCell>hanhan12345</CTableDataCell>
+              <CTableDataCell>2</CTableDataCell>
+              <CTableDataCell>1</CTableDataCell>
+              <CTableDataCell>0</CTableDataCell>
+              <CTableDataCell>1</CTableDataCell>
+              <CTableDataCell>0</CTableDataCell>
+              <CTableDataCell>1</CTableDataCell>
+            </CTableRow>
+            <CTableRow>
+              <CTableDataCell>1</CTableDataCell>
+              <CTableDataCell>
+                <CButton className='info'>Edit akses</CButton>
+              </CTableDataCell>
+              <CTableDataCell>201511041</CTableDataCell>
+              <CTableDataCell>Hanhan Septian</CTableDataCell>
+              <CTableDataCell>hanhan12345</CTableDataCell>
+              <CTableDataCell>2</CTableDataCell>
+              <CTableDataCell>1</CTableDataCell>
+              <CTableDataCell>0</CTableDataCell>
+              <CTableDataCell>1</CTableDataCell>
+              <CTableDataCell>0</CTableDataCell>
+              <CTableDataCell>1</CTableDataCell>
+            </CTableRow>
+            <CTableRow>
+              <CTableDataCell>1</CTableDataCell>
+              <CTableDataCell>
+                <CButton>Edit akses</CButton>
+              </CTableDataCell>
+              <CTableDataCell>201511041</CTableDataCell>
+              <CTableDataCell>Hanhan Septian</CTableDataCell>
+              <CTableDataCell>hanhan12345</CTableDataCell>
+              <CTableDataCell>2</CTableDataCell>
+              <CTableDataCell>1</CTableDataCell>
+              <CTableDataCell>0</CTableDataCell>
+              <CTableDataCell>1</CTableDataCell>
+              <CTableDataCell>0</CTableDataCell>
+              <CTableDataCell>1</CTableDataCell>
+            </CTableRow>
+          </CTableBody>
+        </CTable>
+      </DocsExample>
     </>
   )
 }
--- FrontEnd/FPTLN/src/views/searchfitandpropper/PencarianFitAndPropper.js
+++ FrontEnd/FPTLN/src/views/searchfitandpropper/PencarianFitAndPropper.js
@@ -1,9 +1,61 @@
 import React from 'react'
+import {
+  CButton,
+  CCol,
+  CForm,
+  CFormInput,
+  CFormLabel,
+  CTable,
+  CTableBody,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+} from '@coreui/react'
+import { DocsCallout, DocsExample } from 'src/components'
 
 const PencarianFitAndPropper = () => {
   return (
     <>
-      <p> PencarianFitAndPropper</p>
+      <DocsExample href="forms/layout#gutters">
+        <CForm className="row g-3">
+          <CCol xs={12}>
+            <CFormLabel htmlFor="inputAddress">Input NIP Peserta</CFormLabel>
+            <CFormInput id="inputAddress" placeholder="NIP Peserta" />
+          </CCol>
+          <CCol xs={12}>
+            <CButton type="submit">Cari Peserta</CButton>
+          </CCol>
+        </CForm>
+      </DocsExample>
+      <DocsExample href="components/table">
+        <CTable>
+          <CTableHead>
+            <CTableRow>
+              <CTableHeaderCell scope="col">No</CTableHeaderCell>
+              <CTableHeaderCell scope="col">NIP</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Nama</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Jabatan Proyeksi</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Tanggal Uji</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Hasil Nilai</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Lihat Report Nilai</CTableHeaderCell>
+            </CTableRow>
+          </CTableHead>
+          <CTableBody>
+            <CTableRow>
+              <CTableDataCell>1</CTableDataCell>
+              <CTableDataCell>201511041</CTableDataCell>
+              <CTableDataCell>Hanhan Septian</CTableDataCell>
+              <CTableDataCell>MANAGER SISTEM TRANSMISI</CTableDataCell>
+              <CTableDataCell>09-09-2022</CTableDataCell>
+              <CTableDataCell>Tidak Disaratkan (0)</CTableDataCell>
+              <CTableDataCell>
+                <CButton>Lihat Nilai</CButton>
+              </CTableDataCell>
+            </CTableRow>
+          </CTableBody>
+        </CTable>
+      </DocsExample>
     </>
   )
 }
